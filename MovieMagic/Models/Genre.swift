d9ff3add0fd121334b8a47c43831d99598fc1de6
tharindu senadheera <tharindu.senadeera@ascentic.se>
2023-08-10 21:07:05 +0530

    added genre and info.plist

---
+++ MovieMagic/Models/Genre.swift
@@ -0,0 +1,14 @@
+//
+//  Genres.swift
+//  MovieMagic
+//
+//  Created by Tharindu Senadheera on 8/7/23.
+//
+
+import Foundation
+
+struct Genre: Identifiable, Codable {
+    let id: Int
+    let genre: String
+    let key: String
+}

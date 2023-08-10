2a3d140164404cb5f9efefa7944bd80ed18638d6
tharindu senadheera <tharindu.senadeera@ascentic.se>
2023-08-10 19:26:39 +0530

    Initial Commit

---
+++ MovieMagic/MovieMagicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieMagicApp.swift
+//  MovieMagic
+//
+//  Created by Tharindu Senadheera on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieMagicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
